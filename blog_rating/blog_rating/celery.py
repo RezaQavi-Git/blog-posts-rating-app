a4134ac1b648a74af904e708ebb87924be11ca6f
ReZa QaVi <reza.qavi@tapsi.cab>
2024-12-27 08:03:01 +0330

    fix celery tasks

--- blog_rating/blog_rating/celery.py
+++ blog_rating/blog_rating/celery.py
@@ -2,11 +2,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Set default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_rating.settings')
 
 app = Celery('blog_rating')
 
-# Load task modules from all registered Django app configs.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- blog_rating/posts/tasks.py
+++ blog_rating/posts/tasks.py
@@ -1,10 +1,14 @@
+import datetime
 from celery import shared_task
 from django.db.models import Avg, Count
 from .models import Post
+import logging
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def update_post_metrics():
-    print("here")
+    logger.info(f'Task update_post_metrics is running {datetime.time} ...')
     posts = Post.objects.all()
     for post in posts:
         post.average_rating = post.ratings.aggregate(Avg('score'))['score__avg'] or 0
